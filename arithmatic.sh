916b852b4cee294f15197b4307deb4f5f98249b4
DhanshreePatil01 <patildhanshrees6112@gmail.com>
2022-09-27 20:26:34 +0530

    Added: Taken Expressions

--- arithmatic.sh
+++ arithmatic.sh
@@ -5,4 +5,17 @@ echo "Welcome to Arithmatic Computaion and sorting"
 read -p "Enter the value of a: " a;
 read -p "Enter the value of b: " b;
 read -p "Enter the value of c: " c;
-echo $a,$b,$c
+
+echo "a: " $a;
+echo "b: " $b;
+echo "c: " $c;
+
+expr[0]=$(($a+$b*$c));
+expr[1]=$(($a*$b+$c));
+expr[2]=$(($c+$a/$b));
+expr[3]=$(($a%$b+$c));
+
+echo $expr1;
+echo $expr2;
+echo $expr3;
+echo $expr4;
